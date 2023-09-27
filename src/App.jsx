5cb6a39f5cf38fe994bf5688d5afdcc85788d790
Manav9823 <manavbansalfddk@gmail.com>
2023-09-28 00:34:38 +0530

    basic react app using vite

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Todos from "./todo"
+
+function App() {
+
+  return (
+    <>
+    <Todos></Todos>
+    <h1>Manav study hard</h1>
+    </>
+    
+  )
+}
+
+export default App
---
+++ src/todo.jsx
@@ -0,0 +1,7 @@
+function Todos(){
+    return(
+        <h1> Try to complete all Todo</h1>
+    )
+}
+
+export default Todos;
